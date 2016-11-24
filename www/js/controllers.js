9bd528077d9cc1c0cc35b321315047a6cba64c95
Markus <mkonkol@arcor.de>
2016-11-24 20:16:45 +0100

    Start project

---
+++ www/js/controllers.js
@@ -0,0 +1,42 @@
+angular.module('starter.controllers', [])
+
+.controller('MapCtrl', function($scope, $state) {
+  var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+    var latLng = new google.maps.LatLng(51.962022, 7.624095);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 
+  }, function(error){
+    console.log("Could not get location");
+  })
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
