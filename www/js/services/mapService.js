f4a7cd5a873e20c3eb16da693a0666fffefc022b
Markus <mkonkol@arcor.de>
2016-11-25 20:17:15 +0100

    Routing for two points

---
+++ www/js/services/mapService.js
@@ -0,0 +1,82 @@
+angular.module('starter.services', [])
+
+.factory('mapService', function(){
+	
+	var map = null;
+	var routeElements = [];
+	
+	var initMap = function(){
+		var options = {timeout: 10000, enableHighAccuracy: true};
+	 
+	    var latLng = new google.maps.LatLng(51.962022, 7.624095);
+	 
+	    var mapOptions = {
+	      center: latLng,
+	      zoom: 15,
+	      mapTypeId: google.maps.MapTypeId.ROADMAP
+	    };
+	 
+	    map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	    
+	    var longpress = false;
+
+    	google.maps.event.addListener(map,'click', function (event) {
+        	    (longpress) ? setMarkerForRouting(event.latLng) : null;
+        });
+
+	    google.maps.event.addListener(map, 'mousedown', function(event){
+      	          start = new Date().getTime();           
+        });
+
+    	google.maps.event.addListener(map, 'mouseup', function(event){
+                end = new Date().getTime();
+                    longpress = (end - start < 500) ? false : true;         
+
+        });
+	    
+	    return map;
+	};
+	
+ 	var setMarkerForRouting = function(location){	
+ 		if (routeElements.length < 2){
+	 		var marker = new google.maps.Marker({
+			    position: new google.maps.LatLng(location.lat(), location.lng()),
+			    draggable: true			  	
+			});
+			routeElements.push(marker);
+			for (marker in routeElements){
+				routeElements[marker].setMap(map);
+			}
+			if (routeElements.length == 2) routing();
+		}else{
+			deleteRoutingElements();
+		}
+ 	};
+ 	
+ 	var deleteRoutingElements = function(){
+ 		for (elem in routeElements){
+ 			routeElements[elem].setMap(null);
+ 		}
+ 		routeElements = new Array();
+ 	};
+ 	
+ 	var routing = function(){
+ 		var directionsService = new google.maps.DirectionsService();	
+		var request = {
+		    origin: routeElements[0].position,
+		    destination: routeElements[1].position,
+		    travelMode: google.maps.TravelMode.WALKING,
+		};		
+		directionsService.route(request, function(response, status) {
+		  if (status == google.maps.DirectionsStatus.OK) {
+		    var route = new google.maps.Polyline({path:response.routes[0].overview_path});
+		    route.setMap(map);
+		    routeElements.push(route);
+		  }
+		});			
+ 	};
+	
+	return {
+		initMap: initMap
+	};
+});
